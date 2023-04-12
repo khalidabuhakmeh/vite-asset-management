ca7d877b15efb81e534ba99483bba581d3d43ca8
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-04-12 15:08:52 -0400

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import {defineConfig} from "vite";
+import {processAssetFileNames, entryFileNames, chunkFileNames, assetDir} from "./config/assets";
+import {resolve} from "path";
+import viteHtmlResolveAlias from 'vite-plugin-html-resolve-alias'
+
+export default defineConfig({
+
+    resolve: {
+        alias: {
+            '@img': resolve(__dirname, 'src/img/'),
+            '@rss': resolve(__dirname, 'src/rss.xml')
+        }
+    },
+
+    plugins: [
+      viteHtmlResolveAlias()
+    ],
+
+    build: {
+        minify: true,
+        assetsDir: assetDir,
+        // don't inline anything for demo
+        assetsInlineLimit: 0,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "index.html")
+            },
+            output: {
+                entryFileNames: entryFileNames,
+                assetFileNames: processAssetFileNames,
+                chunkFileNames: chunkFileNames
+            }
+        }
+    }
+});
+
